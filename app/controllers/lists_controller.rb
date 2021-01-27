053edb7bfe52d3f08970b5f9f767d5cd7d30e8ec
Greg Silcox <greg.silcox@gmail.com>
2021-01-27 13:39:57 -0800

    Move common controller actions into a shared concern and adjust views to match

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
+# frozen_string_literal: true
+
 class ListsController < ApplicationController
-  def index
-    @lists = []
+  include BasicActions
+  
+  private
+
+  def model_params
+    params.require(:list).permit(:name)
   end
 end
 
